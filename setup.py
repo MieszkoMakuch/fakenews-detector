d1c071859871d8ce526b8fe923fbef8bab99cb19
Mieszko Makuch <mieszkomakuch@gmail.com>
2017-06-03 14:30:50 +0200

    Added required requirements

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
 from distutils.core import setup
 
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
 setup(
     name='fakenews_detector',
     packages=['fakenews_detector'],
-    version='0.1.4',
+    version='0.1.5',
     description='Detect fake and get information about the source.',
     author='Mieszko Makuch',
     author_email='mmakuch@googlemail.com',
@@ -12,4 +15,6 @@ setup(
     keywords=['fakenews', 'fake', 'news', 'detector'],
     classifiers=[],
     package_data={'fakenews_detector': ['json_local/*', 'json_local/opensources/*']},
+    install_requires=required,
+    zip_safe=False
 )
