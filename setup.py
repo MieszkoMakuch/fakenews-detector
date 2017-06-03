0485f05c84a90f3e811d2f777eacbd69bf699237
Mieszko Makuch <mieszkomakuch@gmail.com>
2017-06-04 01:47:37 +0200

    Added new requirements (this version=0.1.18 builds successfully on heroku)

--- setup.py
+++ setup.py
@@ -1,31 +1,24 @@
-import sys
-import os
+from setuptools import setup
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
-
-if sys.argv[-1] == 'publish':
-    os.system('python3 setup.py sdist upload')  # bdist_wininst
-    sys.exit()
-
-with open('requirements.txt') as f:
-    required = f.read().splitlines()
+# Command to update package python setup.py sdist upload -r pypi
 
 setup(
     name='fakenews_detector',
-    packages=['fakenews_detector'],
-    version='0.1.7',
+    packages=['fakenews_detector', 'fakenews_detector.fake_fact_ai'],
+    version='0.1.18',
     description='Detect fake and get information about the source.',
     author='Mieszko Makuch',
     author_email='mmakuch@googlemail.com',
     url='https://github.com/',
     download_url='https://mieszko_makuch@bitbucket.org/mieszko_makuch/fakenews_detector.git',
     keywords=['fakenews', 'fake', 'news', 'detector'],
-    classifiers=[],
-    package_data={'fakenews_detector': ['json_local/*', 'json_local/opensources/*']},
-    include_package_data=True,
-    install_requires=required,
-    zip_safe=False
+    package_data={'fakenews_detector': ['json_local/*', 'json_local/opensources/*', 'fake_fact_ai/static/*']},
+    zip_safe=False,
+    install_requires=[
+        "validators==0.11.3",
+        "numpy==1.11.3",
+        "scipy==0.18.1",
+        "sklearn==0.0",
+        "nltk==3.2.4"
+    ]
 )
